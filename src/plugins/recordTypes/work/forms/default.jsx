781ef4c869e905e05c42e567009a8bdfe1ac40bd
Ray Lee <rhlee@berkeley.edu>
2018-03-25 01:04:23 -0700

    DRYD-316: Update work form to use address extension.

--- src/plugins/recordTypes/work/forms/default.jsx
+++ src/plugins/recordTypes/work/forms/default.jsx
@@ -15,6 +15,10 @@ const template = (configContext) => {
     InputTable,
   } = configContext.recordComponents;
 
+  const {
+    extensions,
+  } = configContext.config;
+
   return (
     <Field name="document">
       <Panel name="info" collapsible>
@@ -66,39 +70,7 @@ const template = (configContext) => {
           </Col>
         </Cols>
 
-        {/* TODO: Break out address group */}
-
-        <Field name="addrGroupList">
-          <Field name="addrGroup">
-            <Panel>
-              <Cols>
-                <Col>
-                  <Field name="addressPlace1" />
-                  <Field name="addressPlace2" />
-                  <Field name="addressMunicipality" />
-
-                </Col>
-
-                <Col>
-                  <Row>
-                    <Field name="addressStateOrProvince" />
-                    <Field name="addressPostCode" />
-                  </Row>
-
-                  <Field name="addressCountry" />
-
-                  <Row>
-                    <Col>
-                      <Field name="addressType" />
-                    </Col>
-
-                    <Col />
-                  </Row>
-                </Col>
-              </Cols>
-            </Panel>
-          </Field>
-        </Field>
+        {extensions.address.form}
       </Panel>
 
       <Panel name="hierarchy" collapsible collapsed>
