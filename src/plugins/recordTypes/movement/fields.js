7ed26a3b8ba989f1fc1bce62d7fd0fd0bb75cf04
Ray Lee <rhlee@berkeley.edu>
2018-03-27 13:56:56 -0700

    DRYD-346: Add place/local to movement currentLocation source.

---
+++ src/plugins/recordTypes/movement/fields.js
@@ -0,0 +1,21 @@
+export default (configContext) => {
+  const {
+    configKey: config,
+  } = configContext.configHelpers;
+
+  return {
+    document: {
+      'ns2:movements_common': {
+        currentLocation: {
+          [config]: {
+            view: {
+              props: {
+                source: 'location/local,location/offsite,organization/local,place/local',
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+};
