2dcbe71157b19a6645e28319fb1b55e9d0e6320d
Richard Millet <remillet@yahoo.com>
2018-10-29 14:37:39 -0700

    DRYD-507: Added 'cited' term to Title Type field list.

--- src/plugins/recordTypes/collectionobject/optionLists.js
+++ src/plugins/recordTypes/collectionobject/optionLists.js
@@ -1,6 +1,42 @@
 import { defineMessages } from 'react-intl';
 
 export default {
+  titleTypes: {
+    values: [
+      'assigned-by-artist',
+      'collection',
+      'cited',
+      'popular',
+      'series',
+      'trade',
+    ],
+    messages: defineMessages({
+      'assigned-by-artist': {
+        id: 'option.titleTypes.assigned-by-artist',
+        defaultMessage: 'assigned by artist',
+      },
+      collection: {
+        id: 'option.titleTypes.collection',
+        defaultMessage: 'collection',
+      },
+      cited: {
+        id: 'option.titleTypes.cited',
+        defaultMessage: 'cited',
+      },
+      popular: {
+        id: 'option.titleTypes.popular',
+        defaultMessage: 'popular',
+      },
+      series: {
+        id: 'option.titleTypes.series',
+        defaultMessage: 'series',
+      },
+      trade: {
+        id: 'option.titleTypes.trade',
+        defaultMessage: 'trade',
+      },
+    }),
+  },
   creatorTypes: {
     values: [
       'culture',
