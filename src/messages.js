7b4b73aca22120c08aed08e48315c5d7b990588f
Ray Lee <ray.lee@lyrasis.org>
2020-01-11 13:33:00 -0800

    DRYD-796: Update additional messages for searching on all fields.

--- src/messages.js
+++ src/messages.js
@@ -1,62 +1,21 @@
 export default {
-  'about.title': 'Welcome to the CollectionSpace Public Art Demo',
-  'about.contentHTML': `
-    <p>
-      CollectionSpace is a free, open-source collections management application for museums,
-      historical societies, natural science collections, and more.
-    </p>
-    <p>
-      This demo site is running the
-      <a href="http://www.collectionspace.org/current_release/">current version</a> the
-      <a href="https://collectionspace.atlassian.net/wiki/x/BYA_K">public art profile</a> of
-      CollectionSpace, which includes fields and procedures useful for public art collections. If
-      you’re interested in a version of CollectionSpace configured to meet the needs of a different
-      domain, please visit our <a href="https://www.collectionspace.org/demo/">demo landing page</a>
-      to view all available options. Examples include Anthropology, Botanical Garden, Fine Art,
-      Herbarium, and Local History &amp; Material Culture.
-    </p>
-    <p>
-      To log in, use the email <strong>admin@publicart.collectionspace.org</strong>, with the
-      case-sensitive password <strong>Administrator</strong>. To view in read-only mode, use the
-      email <strong>reader@publicart.collectionspace.org</strong> with the password
-      <strong>reader</strong>.
-    </p>
-    <p>
-      Not sure where to get started? Follow along with one of our quick-start guides:
-    </p>
-    <ul>
-      <li>
-        <span>Create a new object:</span>
-        <a href="http://bit.ly/newobjpdf">PDF</a>
-        <a href="https://vimeo.com/112212895">Screencast</a>
-      </li>
-      <li>
-        <span>Link to an image:</span>
-        <a href="http://bit.ly/linkimgpdf">PDF</a>
-        <a href="https://vimeo.com/112214418">Screencast</a>
-      </li>
-      <li>
-        <span>Add a storage location:</span>
-        <a href="http://bit.ly/storlocpdf">PDF</a>
-        <a href="https://vimeo.com/112818120">Screencast</a>
-      </li>
-    </ul>
-    <p>
-      We rebuild this demo site weekly so don't worry, you won't break it!
-    </p>
-  `,
+  'about.title': 'Welcome to CollectionSpace: Public Art',
 
-  'field.concepts_common.scopeNote.name': 'Scope note',
+  // collectionobject
 
   'field.collectionobjects_common.objectNumber.name': 'Artwork ID',
   'field.collectionobjects_common.briefDescription.name': 'Artwork description',
   'field.collectionobjects_common.objectNameGroup.name': 'Work type',
   'field.collectionobjects_common.objectName.fullName': 'Work type',
   'field.collectionobjects_common.objectName.name': 'Type',
-  'field.collectionobjects_common.objectNameSystem.name': 'Classification system',
+  'field.collectionobjects_common.objectNameNote.fullName': 'Work type note',
   'field.collectionobjects_common.responsibleDepartment.name': 'Program name',
   'field.collectionobjects_common.inventoryStatus.name': 'Installation type',
   'field.collectionobjects_common.materialComponentNote.fullName': 'Material note',
   'field.collectionobjects_common.materialComponentNote.name': 'Note',
   'field.groups_common.responsibleDepartment.name': 'Program Name',
+
+  // concept
+
+  'field.concepts_common.scopeNote.name': 'Scope note',
 };
