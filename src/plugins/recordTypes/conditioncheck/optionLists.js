746de7860f674f5a4380fbe59ea14d4313aaf76f
Ray Lee <ray.lee@lyrasis.org>
2023-10-31 18:59:31 -0400

    DRYD-1321: Add values to conditions option list.

--- src/plugins/recordTypes/conditioncheck/optionLists.js
+++ src/plugins/recordTypes/conditioncheck/optionLists.js
@@ -1,6 +1,46 @@
 import { defineMessages } from 'react-intl';
 
 export default {
+  conditions: {
+    values: [
+      'needsnowork',
+      'needs cleaning',
+      'missing hardware',
+      'light/electronic replacement',
+      'environmental issue',
+      'needs coating',
+      'needs repair',
+      'exhibitableneedswork',
+      'notexhibitablestable',
+      'injeopardy',
+    ],
+    messages: defineMessages({
+      'needs cleaning': {
+        id: 'option.conditions.needs cleaning',
+        defaultMessage: 'needs cleaning',
+      },
+      'missing hardware': {
+        id: 'option.conditions.missing hardware',
+        defaultMessage: 'missing hardware',
+      },
+      'light/electronic replacement': {
+        id: 'option.conditions.light/electronic replacement',
+        defaultMessage: 'light/electronic replacement',
+      },
+      'environmental issue': {
+        id: 'option.conditions.environmental issue',
+        defaultMessage: 'environmental issue',
+      },
+      'needs coating': {
+        id: 'option.conditions.needs coating',
+        defaultMessage: 'needs coating',
+      },
+      'needs repair': {
+        id: 'option.conditions.needs repair',
+        defaultMessage: 'needs repair',
+      },
+    }),
+  },
   conditionCheckReasons: {
     values: [
       'cleaning',
