34065b1d072b9182d8609439cc7a3033d939e3d9
Michael Ritter <mikejritter@users.noreply.github.com>
2024-01-19 19:10:29 -0700

    DRYD-1326: Update exit reasons list (#23)

---
+++ src/plugins/recordTypes/objectexit/optionLists.js
@@ -0,0 +1,38 @@
+import { defineMessages } from 'react-intl';
+
+export default {
+  exitReasons: {
+    values: [
+      'deaccession',
+      'disposal',
+      'returnofloan',
+      'temporary',
+      'transfer',
+      'painted out',
+      'covered by construction',
+      'lost to demolition',
+    ],
+    messages: defineMessages({
+      temporary: {
+        id: 'option.exitReasons.temporary',
+        defaultMessage: 'temporary',
+      },
+      transfer: {
+        id: 'option.exitReasons.transfer',
+        defaultMessage: 'transfer',
+      },
+      'painted out': {
+        id: 'option.exitReasons.painted out',
+        defaultMessage: 'painted out',
+      },
+      'covered by construction': {
+        id: 'option.exitReasons.covered by construction',
+        defaultMessage: 'covered by construction',
+      },
+      'lost to demolition': {
+        id: 'option.exitReasons.lost to demolition',
+        defaultMessage: 'lost to demolition',
+      },
+    }),
+  },
+};
