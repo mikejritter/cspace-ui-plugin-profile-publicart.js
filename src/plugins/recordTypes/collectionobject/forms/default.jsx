966d8c394cefce0abbc18874a6770a28453ce1f9
Michael Ritter <emericaskate15@gmail.com>
2024-01-30 12:25:17 -0700

    Add objectCountGroup

--- src/plugins/recordTypes/collectionobject/forms/default.jsx
+++ src/plugins/recordTypes/collectionobject/forms/default.jsx
@@ -94,6 +94,16 @@ const template = (configContext) => {
           </Field>
         </Field>
 
+        <Field name="objectCountGroupList">
+          <Field name="objectCountGroup">
+            <Field name="objectCount" />
+            <Field name="objectCountType" />
+            <Field name="objectCountCountedBy" />
+            <Field name="objectCountDate" />
+            <Field name="objectCountNote" />
+          </Field>
+        </Field>
+
         <Row>
           <Field name="owners">
             <Field name="owner" />
